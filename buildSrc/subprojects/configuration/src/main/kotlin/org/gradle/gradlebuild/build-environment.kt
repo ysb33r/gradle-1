941721936b1766fa398c177deb4d35ab060e9376
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-08-08 00:41:55 -0300

    Upgrade Kotlin DSL `0.19.{4 => 5}-SNAPSHOT`

--- buildSrc/subprojects/configuration/src/main/kotlin/org/gradle/gradlebuild/build-environment.kt
+++ buildSrc/subprojects/configuration/src/main/kotlin/org/gradle/gradlebuild/build-environment.kt
@@ -6,7 +6,7 @@ import org.gradle.internal.os.OperatingSystem
 
 object BuildEnvironment {
     val isCiServer = "CI" in System.getenv()
-    val gradleKotlinDslVersion = "0.19.4-SNAPSHOT"
+    val gradleKotlinDslVersion = "0.19.5-SNAPSHOT"
     val jvm = org.gradle.internal.jvm.Jvm.current()
     val javaVersion = JavaVersion.current()
     val isWindows = OperatingSystem.current().isWindows
